0ceb43b3bb1d9a2d6aa30d63907f1c19d615e58e
Spencer Knight <spencer.knightusn@gmail.com>
2022-02-08 08:16:26 -0700

    created header, added color variables

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
+const homeRouter = require('./routes/homepage');
 const usersRouter = require('./routes/users');
 
 const app = express();
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/', homeRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
