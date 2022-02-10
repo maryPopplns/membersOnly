3bb69a1cfc30454eed8f2cb5875790c961bf8e38
Spencer Knight <spencer.knightusn@gmail.com>
2022-02-09 19:49:58 -0700

    messages showing. mvp complete

--- controllers/homepage.js
+++ controllers/homepage.js
@@ -5,14 +5,40 @@ const debug = require('debug')('membersonly:homepageController');
 
 // [ HOMEPAGE GET ]
 exports.homepage_get = function (req, res, next) {
-  // TODO query messages
   Message.find()
-    .then((results) => {
-      console.log(results);
-      res.render('homepage');
+    .then((messages) => {
+      res.render('homepage', { messages });
     })
     .catch((error) => next(error));
+  // TODO query messages
 };
+
+// [
+//   {
+//     _id: new ObjectId("62044be3ec0db23bc764f4aa"),
+//     title: 'title of the message',
+//     message: 'message body',
+//     date: 2022-02-09T23:18:59.954Z,
+//     author: new ObjectId("6203fbe8b749982172b761d6"),
+//     __v: 0
+//   },
+//   {
+//     _id: new ObjectId("62045023eb35c7a48e74e2bb"),
+//     title: 'numba 2',
+//     message: 'the message',
+//     date: 2022-02-09T23:37:07.616Z,
+//     author: new ObjectId("6203fbe8b749982172b761d6"),
+//     __v: 0
+//   },
+//   {
+//     _id: new ObjectId("620451048daae694fd803a54"),
+//     title: 'adsfa',
+//     message: 'sadfs',
+//     date: 2022-02-09T23:40:52.994Z,
+//     author: new ObjectId("6203fbe8b749982172b761d6"),
+//     __v: 0
+//   }
+// ]
 
 // [ HOMEPAGE POST ]
 exports.homepage_post = [
@@ -37,8 +63,10 @@ exports.homepage_post = [
     );
   },
   function (req, res, next) {
-    console.log('hola');
-    res.render('homepage');
-    // TODO copy from above
+    Message.find()
+      .then((messages) => {
+        res.render('homepage', { messages });
+      })
+      .catch((error) => next(error));
   },
 ];
